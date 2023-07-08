2a393d6b6845a802c9b71109557f1089bb543bce
Wiktor Jasiak <70897386+BlessYouMate@users.noreply.github.com>
2023-07-08 11:54:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const nav = document.querySelector("header");
+const header = document.querySelector("header");
 let lastScrollY = window.scrollY;
+
+
+
+const nav = document.querySelector("nav > ul");
+const button = document.querySelector("button");
 
 window.addEventListener("scroll", () =>{
     if(lastScrollY < window.scrollY){
-        nav.classList.add("nav-hidden");
+        header.classList.add("nav-hidden");
     }
     else{
-        nav.classList.remove("nav-hidden");
+        header.classList.remove("nav-hidden");
     }
     lastScrollY = window.scrollY;
+});
+
+button.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if(visibility === "false"){
+        nav.setAttribute("data-visible", "true");
+    }
+    else{
+        nav.setAttribute("data-visible", "false");
+    }
 });
 
